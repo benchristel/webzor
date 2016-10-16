51a3173cb90f8e18d8f518e66dd3f60f380e3b65
Ben Christel <bchristel@pivotal.io>
2016-10-16 12:10:27 -0700

    Throw an error instead of overwriting function definitions

--- src/lib/define-fn.js
+++ src/lib/define-fn.js
@@ -1,6 +1,9 @@
 define.fn = function(name) {
   return {
     as: function(definition) {
+      if (AppState.definitions.fn[name]) {
+        throw 'define.fn: There is already a defined function named `' + name + '`.'
+      }
       AppState.definitions.fn[name] = definition
     }
   }
--- src/spec/define-fn-spec.js
+++ src/spec/define-fn-spec.js
@@ -8,4 +8,13 @@ describe('define.fn', function() {
     define.fn('fribbity').as(theFunc)
     expect(AppState.definitions.fn.fribbity).toBe(theFunc)
   })
+
+  it('throws an error if the function already exists', function() {
+    var theFunc = function() {}
+    define.fn('fribbity').as(theFunc)
+
+    expect(function() {
+      define.fn('fribbity').as(theFunc)
+    }).toThrow('define.fn: There is already a defined function named `fribbity`.')
+  })
 })
