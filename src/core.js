aa15b34d63bb19019aaefbd2b2925f410db38c79
Ben Christel <bchristel@pivotal.io>
2016-10-16 11:28:19 -0700

    Add doc comments to core.js

--- src/core.js
+++ src/core.js
@@ -1,16 +1,28 @@
 "use strict";
 
+/**
+ * define provides an API for defining functions, matchers,
+ * and other application-level entities.
+ */
 var define = {
   fn: null,
   matcher: null
 }
 Object.seal(define)
 
+/**
+ * test provides an API for testing application entities.
+ */
 var test = {
   fn: null
 }
 Object.seal(test)
 
+/**
+ * AppState holds the definitions of application entities
+ * and other application state. API subject to change, but
+ * may assist in debugging.
+ */
 var AppState = {
   definitions: {
     fn: null,
@@ -23,10 +35,18 @@ var AppState = {
 Object.seal(AppState)
 Object.seal(AppState.definitions)
 
+/**
+ * properties on the Internal object are subject to change,
+ * but may assist application developers in debugging.
+ */
 var Internal = {
   matchers: {}
 }
 
+/**
+ * should is the stable API for accessing matchers.
+ * Currently an alias for AppState.definitions.matchers.
+ */
 var should = null
 
 AppState.reset = function() {
